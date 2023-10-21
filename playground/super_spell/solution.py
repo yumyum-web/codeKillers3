9123de2dc9168dc89f301d22bf1844433a125544
YumYum <yumethsumathipala@gmail.com>
2023-10-21 18:49:05 +0530

    P:Super Spell: Attempt: 2
    All tests Passed

--- playground/super_spell/solution.py
+++ playground/super_spell/solution.py
@@ -1,13 +1,20 @@
-def get_rank(char):
-    return "abcdefghijklmopqrstuvwxyz".index(char)
+def get_lexicographically_smallest(s, c):
+    i = 0
+    while i < len(s):
+        if super_spell[i] > c:
+            s = s[:i] + c + s[i:]
+            return s
+        i = i + 1
+    s = s + c
+    return s
 
 
-def get_char(rank):
-    return "abcdefghijklmopqrstuvwxyz"[rank]
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        super_spell = ""
+        for __ in range(int(input())):
+            chars = list(input())
+            chars.sort()
+            super_spell = get_lexicographically_smallest(super_spell, chars[0])
 
-
-for _ in range(int(input())):
-    super_spell = ""
-    for __ in range(int(input())):
-        super_spell += get_char(min([get_rank(c) for c in input()]))
-    print(super_spell)
+        print(super_spell)
