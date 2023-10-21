72c285edf8429b015e5c38425902330bef15e713
YumYum <yumethsumathipala@gmail.com>
2023-10-21 19:44:34 +0530

    P:Save Rapunzel - I: Score 20

--- playground/save_rapunzel_i/solution.py
+++ playground/save_rapunzel_i/solution.py
@@ -1,15 +1,20 @@
-for _ in range(int(input())):
-    steps = 0
-    current_cell = 0
-    for __ in range(int(input().split(" ")[0])):
-        level = [int(c) for c in input().split(" ")]
-        level.sort()
-        des_start = level[0]
-        des_stop = level[-1]
-        if des_start < current_cell:
-            steps += current_cell - des_start
-            current_cell = des_start
-        if des_stop > current_cell:
-            steps += des_stop - current_cell
-            current_cell = des_stop
-    print(steps)
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        steps = 0
+        start_cell = 0
+        for __ in range(int(input().split(" ")[0])):
+            level = [int(c) for c in input().split(" ")]
+            level.sort()
+            des_start = level[0]
+            des_stop = level[-1]
+            if des_start < start_cell:
+                steps += start_cell - des_start
+                if des_stop <= start_cell:
+                    start_cell = des_start
+                else:
+                    steps += des_stop - des_start
+                    start_cell = des_stop
+            else:
+                steps += des_stop - start_cell
+                start_cell = des_stop
+        print(steps)
